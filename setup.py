748a106e34e3797b0958e6a7c243b7c812cb6f6e
Tyler Young <tyler@x-plane.com>
2018-11-09 14:39:40 -0600

    chore (packaging): Updated metadata for 0.0.1 release

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.md') as f:
     readme_md = f.read()
@@ -6,15 +6,21 @@ with open('README.md') as f:
 setup(
     name='xplane_airports',
     version='0.0.1',
-    packages=['env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.idna', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.pytoml', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.certifi', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.chardet', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.chardet.cli', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.distlib', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.distlib._backport', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.msgpack', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3',
-              'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3.util', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3.contrib', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3.contrib._securetransport', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3.packages', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3.packages.backports', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.urllib3.packages.ssl_match_hostname', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.colorama', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.html5lib', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.html5lib._trie', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.html5lib.filters',
-              'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.html5lib.treewalkers', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.html5lib.treeadapters', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.html5lib.treebuilders', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.lockfile', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.progress', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.requests', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.packaging', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.cachecontrol', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.cachecontrol.caches', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.webencodings',
-              'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._vendor.pkg_resources', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal.req', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal.vcs', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal.utils', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal.models', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal.commands', 'env.lib.python3.7.site-packages.pip-10.0.1-py3.7.egg.pip._internal.operations', 'xplane_airports'],
+    packages=find_packages(),
     url='https://github.com/X-Plane/xplane_airports',
     license='MIT',
     author='Tyler Young',
     author_email='tyler@x-plane.com',
+    python_requires='>=3.7',  # For dataclasses
     description='Tools for manipulating X-Plane\'s apt.dat files & interfacing with the X-Plane Scenery Gateway',
     long_description=readme_md,
     long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Topic :: Games/Entertainment :: Simulation",
+    ],
 )
