1b0f3e4bd239182cce792cb0be9f643288797baa
Tyler Young <tyler@x-plane.com>
2018-11-12 07:56:14 -0600

    chore (versioning): Version bump for docs overhaul

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='xplane_airports',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     url='https://github.com/X-Plane/xplane_airports',
     license='MIT',
