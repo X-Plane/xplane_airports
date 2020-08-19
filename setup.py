5e55eb24bca889d99e8bbbaa57c6ad6921f30170
Tyler Young <tyler@x-plane.com>
2020-08-19 12:28:27 -0500

    chore (version bump): 3.2.1 for release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='xplane_airports',
-    version='3.2.0',
+    version='3.2.1',
     packages=find_packages(),
     url='https://github.com/X-Plane/xplane_airports',
     license='MIT',
